7eccea06c85612897b860afb2d1a7c69a349d779
marco <dobermannpoker86@gmail.com>
2020-03-24 16:57:21 +0100

    meta' strada

---
+++ esercizio.js
@@ -0,0 +1,36 @@
+// console.log("ciao");
+//lista cognomi, ordinamento e posizione cognome inserito
+//DESCRIZIONE:
+//Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+// chiedere cognome utente
+var cognomeUtente = prompt("inserisca il suo cognome");
+//
+// console.log("grazie e benvenuti");
+
+//inserirlo in una erray con altri cognomi + cognomeUtente
+var listaCognomi = ["castro", "basso","astotele" , "giletti", "zontana" , + "" + cognomeUtente];
+
+
+// console.log(listaCognomi);
+
+//variabili aggiuntivi
+var itemN , vecchioContenuto ,ordine ;
+
+//stampare lista di cognomi in ordine alfabetico con  la posizione del nuovo utente
+
+for (var i = 0 ; i < listaCognomi.length ; i++) {
+    itemN = listaCognomi[i];
+    console.log(itemN);
+
+    //diamo un ordine alla lista
+    function myFunction() {
+        listaCognomi.sort;
+        document.getElementById("nomignoli").innerHTML = listaCognomi;
+    }
+
+    vecchioContenuto = document.getElementById('nomignoli').innerHTML;
+    // console.log("iterazione numero : " + 1 + vecchioContenuto);
+    document.getElementById('nomignoli').innerHTML = vecchioContenuto + "<li>" + itemN + "</li>";
+}
